e5e95b9c6f294d131f66cfc59131a1e041fc2c66
Surayyo <rashidovas1995@gmail.com>
2026-06-21 23:26:53 +0500

    test

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -14,6 +14,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
